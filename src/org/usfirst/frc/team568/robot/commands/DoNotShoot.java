b0eb6f174e1cfc82d969a99b8a3f444aee523db4
CalebBell1 <Calebbell20142gmail.com>
2016-02-17 14:26:27 -0900

    Shooter code

---
+++ src/org/usfirst/frc/team568/robot/commands/DoNotShoot.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc.team568.robot.commands;
+
+import org.usfirst.frc.team568.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class DoNotShoot extends Command {
+
+	public DoNotShoot() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+		Robot.getInstance().tankDrive.stopShooter();
+	}
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/org/usfirst/frc/team568/robot/commands/Shoot.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc.team568.robot.commands;
+
+import org.usfirst.frc.team568.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class Shoot extends Command {
+
+	public Shoot() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+		Robot.getInstance().tankDrive.shoot();
+	}
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
