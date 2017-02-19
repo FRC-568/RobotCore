1757570360dd0f2a9961bd4f9c503fbaefddc6ac
mackb <mackb@DESKTOP-88OK59D.asdk12.org>
2017-02-19 11:35:06 -0900

    combined dn with ak

--- RobotCore-AWK/src/org/usfirst/frc/team568/robot/subsystems/WinchClimber.java
+++ RobotCore-AWK/src/org/usfirst/frc/team568/robot/subsystems/WinchClimber.java
@@ -2,7 +2,6 @@ package org.usfirst.frc.team568.robot.subsystems;
 
 import org.usfirst.frc.team568.robot.Robot;
 import org.usfirst.frc.team568.robot.RobotMap;
-import org.usfirst.frc.team568.robot.commands.BringTheRobotDown;
 import org.usfirst.frc.team568.robot.commands.ClimbWithWinch;
 
 import edu.wpi.first.wpilibj.SpeedController;
@@ -15,7 +14,7 @@ public class WinchClimber {
 		climber = new Victor(RobotMap.climber);
 
 		Robot.getInstance().oi.climb.whileHeld(new ClimbWithWinch());
-		Robot.getInstance().oi.inverseClimb.whileHeld(new BringTheRobotDown());
+
 	}
 
 }
