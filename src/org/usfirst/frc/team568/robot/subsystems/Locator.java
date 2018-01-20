0177473cc20bb9105fec59f23d7d2d130896142b
Bionicman16 <pinedavince0@gmail.com>
2018-01-20 13:25:33 -0900

    Added Locator subsystem class

---
+++ src/org/usfirst/frc/team568/robot/subsystems/Locator.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team568.robot.subsystems;
+
+import org.usfirst.frc.team568.robot.PortMapper;
+
+public class Locator extends SubsystemBase {
+
+	public Locator(PortMapper source) {
+		super(source);
+		// TODO Auto-generated constructor stub
+	}
+
+}
