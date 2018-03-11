9fb906c8c1b7e8c4660c0172b40afc2abcf0f416
Allen Koch <mackboy1999@yahoo.com>
2018-03-11 11:38:31 -0700

    Auto Two

---
+++ src/org/usfirst/frc/team568/robot/powerup/AutoTwo.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team568.robot.powerup;
+
+import org.usfirst.frc.team568.robot.commands.Drive2018;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class AutoTwo extends CommandGroup {
+	AutoTwo(Robot robot) {
+		addSequential(new Drive2018(robot.getSubsystem(DriveTrain2018.class), 120, .3));
+		// addSequential(new Turn2018(robot.getSubsystem(DriveTrain2018.class), 90));
+	}
+}
