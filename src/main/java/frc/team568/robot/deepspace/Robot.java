7ccd1c2c9154bf543b6fb2ebc0e60c941d70c498
Matthew Johnson <mlucjuly2016@gmail.com>
2019-02-02 11:08:44 -0900

    updated for new drive and camera

--- src/main/java/frc/team568/robot/deepspace/Robot.java
+++ src/main/java/frc/team568/robot/deepspace/Robot.java
@@ -1,59 +1,89 @@
 package frc.team568.robot.deepspace;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import org.opencv.core.Mat;
+
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import frc.team568.robot.RobotBase;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class Robot extends RobotBase {
 	Command autonomousCommand;
 
 	private Compressor compressor;
 
+	//motor ports
+	private static final int lfMotorPort = 1;
+	//private static final int lbMotorPort = 2;
+  	//private static final int rfMotorPort = 3;
+	private static final int rbMotorPort = 2;
+
+	// //joystick inputs
+	//private static final int JoystickInput = 1;
+	//private static final int rightJoystickInput = 1;
+
+	public DifferentialDrive drive;
+	
+	public Joystick controller1;
+	public Joystick controller2;
+	
+	boolean isShifted;
+	boolean shiftIsHeld;
+
+	DoubleSolenoid solenoidShifter;
+	
+	UsbCamera camera;
+	GripPipeline gripPipeline;
+	CvSink cvSink;
+	Mat mat;
+
 	public Timer timer;
 
 	DriveTrain2019 driveTrain;
-
-	Camera camera;
 
 	// Camera server port
 	private final int cameraServerPort = 0;
 
 	public Robot() {
-		// Joystick ports
-		port("mainJoystick", 1);
-		port("cameraJoystick", 0);
-
-		// Motor ports
-		port("leftFrontMotor", 1);
-		port("leftBackMotor", 2);
-		port("rightFrontMotor", 3);
-		port("rightBackMotor", 4);
-
-		// Servo ports
-		port("verticalServo", 0);
-		port("horizontalServo", 1);
-
-		// Double solenoid ports
-		port("forwardChannel", 0);
-		port("reverseChannel", 1);
-
-		// Camera
-		camera = addSubsystem(Camera::new);
-
-		// Drivetrain
-		driveTrain = addSubsystem(DriveTrain2019::new);
-
 		// Compressor
 		compressor = new Compressor();
+		WPI_TalonSRX frontLeft = new WPI_TalonSRX(lfMotorPort);	
+		//WPI_TalonSRX rearLeft = new WPI_TalonSRX(lbMotorPort);
+		//WPI_TalonSRX frontRight = new WPI_TalonSRX(rfMotorPort);
+		WPI_TalonSRX rearRight = new WPI_TalonSRX(rbMotorPort);
+
+		drive = new DifferentialDrive(frontLeft, rearRight);
+		frontLeft.setInverted(true);
+		//rearLeft.setInverted(true);
+		//frontRight.setInverted(true);
+		rearRight.setInverted(true);
+		// rightStick = new Joystick(JoystickInput);
+		// leftStick = new Joystick(rightJoystickInput);
+		controller1 = new Joystick(0);
+    	controller2 = new Joystick(1);
 	}
 
 	@Override
 	public void robotInit() {
 		compressor.start();
 		CameraServer.getInstance().startAutomaticCapture(cameraServerPort);
+		camera = CameraServer.getInstance().startAutomaticCapture();
+		gripPipeline = new GripPipeline();
+		cvSink = CameraServer.getInstance().getVideo();
+		mat = new Mat();
+		
+		solenoidShifter = new DoubleSolenoid(0, 1);
+		
+		compressor = new Compressor();
 	}
 
 	@Override
@@ -63,23 +93,42 @@ public class Robot extends RobotBase {
 
 	@Override
 	public void autonomousInit() {
-
+		compressor.setClosedLoopControl(true);
 	}
 
 	@Override
 	public void autonomousPeriodic() {
 		Scheduler.getInstance().run();
+
+		//drive.tankDrive(0.5, 0.5);
+		cvSink.grabFrame(mat);
+		gripPipeline.process(mat);
 	}
 
 	@Override
 	public void teleopInit() {
-
+		compressor.setClosedLoopControl(true);
 	}
 
 	@Override
 	// Called every 20 milliseconds in teleop
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
+	
+		drive.tankDrive(controller1.getRawAxis(1), controller1.getRawAxis(5));
+
+		if(controller1.getRawButton(4) ) {	
+			if(!shiftIsHeld) {
+				if(isShifted) {
+					solenoidShifter.set(DoubleSolenoid.Value.kReverse); //high gear
+				} else {									
+					solenoidShifter.set(DoubleSolenoid.Value.kForward); //low gear
+				}
+			}
+			shiftIsHeld = true;
+		} else {
+			shiftIsHeld = false;
+		}	
 	}
 
 	@Override
@@ -98,6 +147,8 @@ public class Robot extends RobotBase {
 			autonomousCommand.cancel();
 			autonomousCommand = null;
 		}
+
+		compressor.setClosedLoopControl(false);
 	}
 
 	@Override
---
+++ src/main/java/frc/team568/robot/deepspace/VisionTargetTracker.java
@@ -0,0 +1,118 @@
+/**
+ * Based on code from https://github.com/TheGuyWhoCodes/LiftTracker
+ */
+
+package frc.team568.robot.deepspace;
+
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.imgproc.Imgproc;
+import frc.team568.grip.GearLifterTarget;
+
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public final class VisionTargetTracker extends Subsystem {
+	public static final double OFFSET_TO_FRONT = 0;
+	public static final int CAMERA_WIDTH = 320; // 640;
+	public static final int CAMERA_HEIGHT = 240; // 480;
+	public static final double DISTANCE_CONSTANT = 5760; // 5738;
+	public static final double WIDTH_BETWEEN_TARGET = 8.5; // inches
+
+	public final String cameraName;
+	public final UsbCamera camera;
+
+	private GripPipeline pipeline;
+	private Mat matOriginal;
+	private double lengthBetweenContours;
+	private double distanceFromTarget;
+	private double centerX;
+
+	public VisionTargetTracker() {
+		this(0);
+	}
+
+	public VisionTargetTracker(final int cameraUsbPort) {
+		matOriginal = new Mat();
+		pipeline = new GripPipeline();
+		camera = CameraServer.getInstance().startAutomaticCapture(cameraUsbPort);
+		cameraName = camera.getName();
+		camera.setResolution(CAMERA_WIDTH, CAMERA_HEIGHT);
+	}
+
+	public void processImage() {
+		CameraServer.getInstance().getVideo(cameraName).grabFrame(matOriginal);
+		pipeline.process(matOriginal);
+		returnCenterX();
+	}
+
+	public double returnCenterX() {
+		// This is the center value returned by GRIP thank WPI
+		if (!pipeline.filterContoursOutput().isEmpty()) {
+			Rect r = Imgproc.boundingRect(pipeline.filterContoursOutput().get(1));
+			//Rect r1 = Imgproc.boundingRect(pipeline.filterContoursOutput.get(0));
+			centerX = r.x + (r.width / 2);
+			// this again checks for the 2 shapes on the target
+			// if (centerX.length == 2) {
+			// 	// subtracts one another to get length in pixels
+			// 	lengthBetweenContours = Math.abs(centerX[0] - centerX[1]);
+			//}
+		}
+		return centerX;
+	}
+
+	public double distanceFromTarget() {
+		// distance constant divided by length between centers of contours
+		distanceFromTarget = DISTANCE_CONSTANT / lengthBetweenContours;
+		return distanceFromTarget - OFFSET_TO_FRONT;
+	}
+
+	// public double getAngle() {
+	// 	// 8.5in is for the distance from center to center from goal, then
+	// 	// divide by lengthBetweenCenters in pixels to get proportion
+	// 	double constant = WIDTH_BETWEEN_TARGET / lengthBetweenContours;
+	// 	double angleToGoal = 0;
+	// 	// Looking for the 2 blocks to actually start trig
+	// 	if (!pipeline.filterContoursOutput.isEmpty() && pipeline.filterContoursOutput.size() >= 2) {
+
+	// 		if (centerX.length == 2) {
+	// 			// this calculates the distance from the center of goal to
+	// 			// center of webcam
+	// 			double distanceFromCenterPixels = ((centerX[0] + centerX[1]) / 2) - (CAMERA_WIDTH / 2);
+	// 			// Converts pixels to inches using the constant from above.
+	// 			double distanceFromCenterInch = distanceFromCenterPixels * constant;
+	// 			// math brought to you buy Chris and Jones
+	// 			angleToGoal = Math.atan(distanceFromCenterInch / distanceFromTarget());
+	// 			angleToGoal = Math.toDegrees(angleToGoal);
+	// 			// prints angle
+	// 			// System.out.println("Angle: " + angleToGoal);
+	// 		}
+	// 	}
+	// 	SmartDashboard.putNumber("angleToGoal", angleToGoal);
+	// 	return angleToGoal;
+	// }
+
+	@Override
+	protected void initDefaultCommand() {
+		class ImageProcessor extends Command {
+			public ImageProcessor() {
+				requires(VisionTargetTracker.this);
+			}
+
+			@Override
+			protected void execute() {
+				// processImage();
+			}
+
+			@Override
+			protected boolean isFinished() {
+				return false;
+			}
+		}
+		setDefaultCommand(new ImageProcessor());
+	}
+
+}
