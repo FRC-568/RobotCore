269db4147ee89fff45875cd7f0389061e190096f
Danny Tix <dannytix@gmail.com>
2019-02-09 15:41:13 -0900

    Revert temp changes to Main

--- src/main/java/frc/team568/robot/Main.java
+++ src/main/java/frc/team568/robot/Main.java
@@ -2,7 +2,7 @@ package frc.team568.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
 
-import frc.team568.robot.bart.Robot;
+import frc.team568.robot.deepspace.Robot;
 
 public final class Main {
 	private Main() {}
