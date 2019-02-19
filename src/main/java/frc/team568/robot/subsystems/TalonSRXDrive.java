a51462955ca86cf38080884c275fb0302f001e93
Toya Takahashi <toyatakahashi@Takahashis-MacBook-Air.local>
2019-02-18 18:08:13 -0900

    added gyro subsystem and PID controller

--- src/main/java/frc/team568/robot/subsystems/TalonSRXDrive.java
+++ src/main/java/frc/team568/robot/subsystems/TalonSRXDrive.java
@@ -3,6 +3,10 @@ package frc.team568.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -10,15 +14,25 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 import frc.team568.robot.RobotBase;
 
 public class TalonSRXDrive extends DriveBase {
+	private static final double Kp = 0.2;
+	private static final double Ki = 0;
+	private static final double Kd = 0.1;
+
 	private final DifferentialDrive drive;
 	private WPI_TalonSRX[] motorsL;
 	private WPI_TalonSRX[] motorsR;
+	private PIDController pidController;
+	private double errorRate;
 
 	public TalonSRXDrive(final RobotBase robot) {
 		super(robot);
 		drive = buildDrive();
 		configureEncoder(motorsL[0]);
 		configureEncoder(motorsR[0]);
+
+		pidController = new PIDController(Kp, Ki, Kd, new DriftCalculator(), new ErrorRate());
+		pidController.enable();
+		addChild(pidController);
 	}
 
 	public TalonSRXDrive(String name, final RobotBase robot) {
@@ -26,6 +40,34 @@ public class TalonSRXDrive extends DriveBase {
 		drive = buildDrive();
 		configureEncoder(motorsL[0]);
 		configureEncoder(motorsR[0]);
+	}
+
+	private class DriftCalculator implements PIDSource {
+
+		@Override
+		public PIDSourceType getPIDSourceType() {
+			return PIDSourceType.kRate;
+		}
+
+		@Override
+		public double pidGet() {
+			double MAX_VELOCITY = 3000;
+			double diff = getVelocity(Side.LEFT) - getVelocity(Side.RIGHT);
+			return diff / MAX_VELOCITY;
+		}
+
+		@Override
+		public void setPIDSourceType(PIDSourceType arg0) {}
+
+	}
+
+	private class ErrorRate implements PIDOutput {
+
+		@Override
+		public void pidWrite(double arg0) {
+			errorRate = arg0;
+		}
+
 	}
 
 	private DifferentialDrive buildDrive() {
@@ -37,7 +79,7 @@ public class TalonSRXDrive extends DriveBase {
 		motorsL = new WPI_TalonSRX[ports.length];
 		for (int i = 0; i < motorsL.length; i++) {
 			motorsL[i] = new WPI_TalonSRX(ports[i]);
-			//addChild(motorsL[i]);
+			// addChild(motorsL[i]);
 			motorsL[i].setInverted(invert);
 			if (i > 0)
 				motorsL[i].follow(motorsL[0]);
@@ -48,7 +90,7 @@ public class TalonSRXDrive extends DriveBase {
 		motorsR = new WPI_TalonSRX[ports.length];
 		for (int i = 0; i < motorsR.length; i++) {
 			motorsR[i] = new WPI_TalonSRX(ports[i]);
-			//addChild(motorsR[i]);
+			// addChild(motorsR[i]);
 			motorsR[i].setInverted(invert);
 			if (i > 0)
 				motorsR[i].follow(motorsR[0]);
@@ -92,28 +134,22 @@ public class TalonSRXDrive extends DriveBase {
 
 	@Override
 	public double getVelocity() {
-		return (motorsL[0].getSelectedSensorVelocity()
-			+ motorsR[0].getSelectedSensorVelocity()) / 2;
+		return (motorsL[0].getSelectedSensorVelocity() + motorsR[0].getSelectedSensorVelocity()) / 2;
 	}
 
 	@Override
 	public double getVelocity(Side side) {
-		return side == Side.RIGHT
-			? motorsR[0].getSelectedSensorVelocity()
-			: motorsL[0].getSelectedSensorVelocity();
+		return side == Side.RIGHT ? motorsR[0].getSelectedSensorVelocity() : motorsL[0].getSelectedSensorVelocity();
 	}
 
 	@Override
 	public double getDistance() {
-		return (motorsL[0].getSelectedSensorPosition()
-			+ motorsR[0].getSelectedSensorPosition()) / 2;
+		return (motorsL[0].getSelectedSensorPosition() + motorsR[0].getSelectedSensorPosition()) / 2;
 	}
 
 	@Override
 	public double getDistance(Side side) {
-		return side == Side.RIGHT
-			? motorsR[0].getSelectedSensorPosition()
-			: motorsL[0].getSelectedSensorPosition();
+		return side == Side.RIGHT ? motorsR[0].getSelectedSensorPosition() : motorsL[0].getSelectedSensorPosition();
 	}
 
 	@Override
@@ -121,7 +157,7 @@ public class TalonSRXDrive extends DriveBase {
 		motorsL[0].setSelectedSensorPosition(0);
 		motorsR[0].setSelectedSensorPosition(0);
 	}
-	
+
 	@Override
 	protected void initDefaultCommand() {
 		setDefaultCommand(new Command() {
@@ -129,10 +165,13 @@ public class TalonSRXDrive extends DriveBase {
 			boolean safeMode = configBoolean("enableSafeMode");
 			boolean alreadyToggled = false;
 
-			{ requires(TalonSRXDrive.this); }
+			{
+				requires(TalonSRXDrive.this);
+			}
 
 			@Override
 			protected void initialize() {
+				pidController.enable();
 				System.out.println("Beginning teleop control - safemode is " + (safeMode ? "Enabled" : "Disabled")
 						+ ". Hold L3 + R3 5 seconds to toggle safe mode.");
 			}
@@ -152,11 +191,13 @@ public class TalonSRXDrive extends DriveBase {
 					alreadyToggled = false;
 				}
 
+				pidController.setSetpoint(axis("turn"));
+
 				if (safeMode)
 					arcadeDrive(-axis("forward") * 0.5, axis("turn") * 0.5);
 				else
-					arcadeDrive(-axis("forward"), axis("turn") * 0.6);
-					
+					arcadeDrive(-axis("forward"), axis("turn") * 0.7);
+
 				if (button("stopMotors")) {
 					drive.stopMotor();
 				} else if (button("idleMotors")) {
@@ -176,6 +217,8 @@ public class TalonSRXDrive extends DriveBase {
 		super.initSendable(builder);
 		builder.addDoubleProperty("Left Velocity", () -> getVelocity(Side.LEFT), null);
 		builder.addDoubleProperty("Right Velocity", () -> getVelocity(Side.RIGHT), null);
+		builder.addDoubleProperty("Average Distance", () -> getDistance(), null);
+		builder.addDoubleProperty("PiDErrorRate", () -> errorRate, null);
 	}
 
 }
