265e180b4cfcc13bad75689e673a5a857d0e3e00
Toya Takahashi <toyatakahashi522@gmail.com>
2020-01-14 19:57:45 -0900

    Deleted setTolerance for PIDdrive

--- src/main/java/frc/team568/robot/Main.java
+++ src/main/java/frc/team568/robot/Main.java
@@ -1,8 +1,7 @@
 package frc.team568.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
-
-import frc.team568.robot.deepspace.Robot;
+import frc.team568.robot.bart.Robot;
 
 public final class Main {
 	private Main() {}
