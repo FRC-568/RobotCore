f0be1b71271e33963f77d9cf49fec718edcf7c1e
SKero2004 <toyatakahashi522@gmail.com>
2020-02-15 12:10:10 -0800

    Changed main file to recharge

--- src/main/java/frc/team568/robot/Main.java
+++ src/main/java/frc/team568/robot/Main.java
@@ -1,7 +1,7 @@
 package frc.team568.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
-import frc.team568.robot.deepspace.Robot;
+import frc.team568.robot.recharge.Robot;
 
 public final class Main {
 	private Main() {}
