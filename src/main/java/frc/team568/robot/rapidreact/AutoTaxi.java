6667ee26891b08de748b928ef11a151b3b8904cc
Danny <dannytix@gmail.com>
2022-03-04 17:41:16 -0900

    Auto should not finish immediately

--- src/main/java/frc/team568/robot/rapidreact/AutoTaxi.java
+++ src/main/java/frc/team568/robot/rapidreact/AutoTaxi.java
@@ -1,77 +1,40 @@
 package frc.team568.robot.rapidreact;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 
-//created taxi class
 public class AutoTaxi extends SequentialCommandGroup {
 	protected MecanumSubsystem subsystem;
 
-public AutoTaxi(MecanumSubsystem subsystem) {
+	public AutoTaxi(MecanumSubsystem subsystem) {
 		this.subsystem = subsystem;
-		this.addCommands(
+		addRequirements(subsystem);
+		addCommands(
 				new WaitUntilCommand(10),
 				new RunCommand(() -> {
-					// to move
-					/*
-					 * subsystem.motorFL.set(1);
-					 * subsystem.motorFR.set(1);
-					 * subsystem.motorBL.set(1);
-					 * subsystem.motorBR.set(1);
-					 */
-					subsystem.getMecanumDrive().driveCartesian(0, 1, 0);
-				}).withTimeout(1), new InstantCommand(() -> {
-					// to stop
-					/*
-					 * subsystem.motorFL.set(0);
-					 * subsystem.motorFR.set(0);
-					 * subsystem.motorBL.set(0);
-					 * subsystem.motorBR.set(0);
-					 */
-					subsystem.getMecanumDrive().driveCartesian(0, 0, 0);
-				}));
-		addRequirements(subsystem);
+					subsystem.getMecanumDrive().driveCartesian(0, 1, 0); // to move
+				}).withTimeout(1));
 	}
 
 	@Override
 	public void end(boolean interrupted) {
+		subsystem.getMecanumDrive().driveCartesian(0, 0, 0);
+		super.end(interrupted);
 	}
 
-	// made motors move for one second (reformated)
-	/*
-	 * @Override
-	 * ublic void execute() {
-	 * subsystem.motorFL.set(1);
-	 * subsystem.motorFR.set(1);
-	 * subsystem.motorBL.set(1);
-	 * subsystem.motorBR.set(1);
-	 * 
-	 * try {
-	 * wait(1000);
-	 * } catch (InterruptedException e) {
-	 * e.printStackTrace();
-	 * }
-	 * subsystem.motorFL.set(0);
-	 * subsystem.motorFR.set(0);
-	 * subsystem.motorBL.set(0);
-	 * subsystem.motorBR.set(0);
-	 * 
-	 * }
-	 */
+	@Override
+	public void execute() {
+		super.execute();
+	}
 
 	@Override
 	public void initialize() {
+		super.initialize();
 	}
 
 	@Override
 	public boolean isFinished() {
-		return true;
-		// this is a one line comment.
-		/*
-		 * this is a multiline commment
-		 * return false;
-		 */
+		return super.isFinished();
 	}
 }
