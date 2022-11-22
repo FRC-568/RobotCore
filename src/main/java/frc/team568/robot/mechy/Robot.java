72866f647b4791406376f17477f5f2ac917709ce
consla <conslaaa@proton.me>
2022-11-22 12:47:00 -0900

    fix(mechy): swap drivetrain port numbers

--- src/main/java/frc/team568/robot/mechy/Robot.java
+++ src/main/java/frc/team568/robot/mechy/Robot.java
@@ -1,13 +1,13 @@
 package frc.team568.robot.mechy;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.team568.robot.RobotBase;
 import frc.team568.robot.Xinput;
 import frc.team568.robot.subsystems.MechyDrive;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class Robot extends RobotBase {
 	
@@ -22,8 +22,8 @@ public class Robot extends RobotBase {
 		JoystickButton joystickStart = new JoystickButton(new Joystick(mainJoystick), Xinput.Start);
 
 		port("leftFrontMotor", 1);
-		port("rightFrontMotor", 2);
-		port("leftBackMotor", 3);
+		port("rightFrontMotor", 3);
+		port("leftBackMotor", 2);
 		port("rightBackMotor", 4);
 
 		axis("forward", mainJoystick, Xinput.LeftStickY);
