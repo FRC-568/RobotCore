627e35701125e02ea3db36a5212b2e2a5224de81
consla <conslaaa@proton.me>
2023-02-20 14:14:10 -0900

    Add LiftSubsystem data to dashboard

--- src/main/java/frc/team568/robot/chargedup/LiftSubsystem.java
+++ src/main/java/frc/team568/robot/chargedup/LiftSubsystem.java
@@ -6,9 +6,7 @@ import com.ctre.phoenix.motorcontrol.*;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -21,14 +19,10 @@ public class LiftSubsystem extends SubsystemBase {
     // === stage ===
 	private WPI_TalonSRX stageMotor;
     private DigitalInput limitSwitch1;
-	private ElevatorFeedforward feedforward1;
+	private static double feedforward1 = 0.0;
+    private static double accel1 = 1024;
+    private static double maxV1 = 2048;
 	// TODO: figure out max vel + accel w/ feedforward
-
-    // feedforward constants
-	private static double kStatic1 = 0.0;
-	private static double kV1 = 0.0;
-	private static double kA1 = 0.0;
-	private static double kG1 = 0.0;
 
     // pid constants
 	private static double kP1 = 0.0;
@@ -38,27 +32,21 @@ public class LiftSubsystem extends SubsystemBase {
     // === carriage ===
 	private WPI_TalonSRX carriageMotor;
 	private DigitalInput limitSwitch2;
-	private ElevatorFeedforward feedforward2;
+	private static double feedforward2 = 0.0;
+    private static double accel2 = 1024;
+    private static double maxV2 = 2048;
 	// TODO: figure out max vel + accel w/ feedforward
-
-    // feedforward constants
-	private static double kStatic2 = 0.0;
-	private static double kV2 = 0.0;
-	private static double kA2 = 0.0;
-	private static double kG2 = 0.0;
 
     // pid constants
 	private static double kP2 = 0.0;
 	private static double kI2 = 0.0;
 	private static double kD2 = 0.0;
 
+    private static double neutralDeadband = 0.001;
     // all the way down, all the way up
     private static double[] STAGE_LEVELS = {0, 4096};
     // intake, collected, and outtake
 	private static double[] CARRIAGE_LEVELS = {0, 4096, 8192};
-
-	private Runnable setter;
-	boolean override = false;
 
 	public LiftSubsystem(int stagePort, int carriagePort, int switchPort1, int switchPort2) {
 		// liftMotor = new WPI_TalonSRX(motorPort);
@@ -66,14 +54,13 @@ public class LiftSubsystem extends SubsystemBase {
 		addChild("stageMotor", stageMotor);
         limitSwitch1 = new DigitalInput(switchPort1);
 		addChild("limitSwitch1", limitSwitch1);
-		feedforward1 = new ElevatorFeedforward(kStatic1, kG1, kV1, kA1);
 
         stageMotor.set(ControlMode.PercentOutput, 0);
         stageMotor.setNeutralMode(NeutralMode.Brake);
         stageMotor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 30);
-		stageMotor.configNeutralDeadband(0.001);
-		stageMotor.configMotionAcceleration(1024);
-		stageMotor.configMotionCruiseVelocity(2048);
+		stageMotor.configNeutralDeadband(neutralDeadband);
+		stageMotor.configMotionAcceleration(accel1);
+		stageMotor.configMotionCruiseVelocity(maxV1);
 		// what is the slot index?
 		stageMotor.config_kP(0, kP1);
 		stageMotor.config_kI(0, kI1);
@@ -83,15 +70,13 @@ public class LiftSubsystem extends SubsystemBase {
 		addChild("carriageMotor", carriageMotor);
         limitSwitch2 = new DigitalInput(switchPort2);
 		addChild("limitSwitch2", limitSwitch2);
-		feedforward2 = new ElevatorFeedforward(kStatic2, kG2, kV2, kA2);
 
 		carriageMotor.set(ControlMode.PercentOutput, 0);
         carriageMotor.setNeutralMode(NeutralMode.Brake);
-		/// i don't know what i'm doing here
-        // carriageMotor.configRemoteFeedbackFilter(, RemoteSensorSource.CANCoder, 0);
-		carriageMotor.configNeutralDeadband(0.001);
-		carriageMotor.configMotionAcceleration(1024);
-		carriageMotor.configMotionCruiseVelocity(2048);
+		carriageMotor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 30);
+		carriageMotor.configNeutralDeadband(neutralDeadband);
+		carriageMotor.configMotionAcceleration(accel2);
+		carriageMotor.configMotionCruiseVelocity(maxV2);
 		carriageMotor.config_kP(0, kP2);
 		carriageMotor.config_kI(0, kI2);
 		carriageMotor.config_kD(0, kD2);
@@ -107,57 +92,49 @@ public class LiftSubsystem extends SubsystemBase {
 		override = false;
         switch (level) {
             case 0:
-				setter = () -> {
-						stageMotor.set(ControlMode.MotionMagic,
-									STAGE_LEVELS[0],
+				stageMotor.set(ControlMode.MotionMagic,
+							STAGE_LEVELS[0],
+							DemandType.ArbitraryFeedForward,
+							feedforward1);
+				carriageMotor.set(ControlMode.MotionMagic,
+									CARRIAGE_LEVELS[0],
 									DemandType.ArbitraryFeedForward,
-									feedforward1.calculate(stageMotor.getActiveTrajectoryVelocity()));
-						carriageMotor.set(ControlMode.MotionMagic,
-										  CARRIAGE_LEVELS[0],
-										  DemandType.ArbitraryFeedForward,
-										  feedforward2.calculate(carriageMotor.getActiveTrajectoryVelocity()));
-						};
+									feedforward2);
 				break;
 			case 1:
-				setter = () -> {
-						stageMotor.set(ControlMode.MotionMagic,
-									STAGE_LEVELS[0],
+				stageMotor.set(ControlMode.MotionMagic,
+							STAGE_LEVELS[0],
+							DemandType.ArbitraryFeedForward,
+							feedforward1);
+				carriageMotor.set(ControlMode.MotionMagic,
+									CARRIAGE_LEVELS[1],
 									DemandType.ArbitraryFeedForward,
-									feedforward1.calculate(stageMotor.getActiveTrajectoryVelocity()));
-						carriageMotor.set(ControlMode.MotionMagic,
-										  CARRIAGE_LEVELS[1],
-										  DemandType.ArbitraryFeedForward,
-										  feedforward2.calculate(carriageMotor.getActiveTrajectoryVelocity()));
-						};
+									feedforward2);
 				break;
 			case 2:
-				setter = () -> {
-						stageMotor.set(ControlMode.MotionMagic,
-									STAGE_LEVELS[0],
+				stageMotor.set(ControlMode.MotionMagic,
+							STAGE_LEVELS[0],
+							DemandType.ArbitraryFeedForward,
+							feedforward1);
+				carriageMotor.set(ControlMode.MotionMagic,
+									CARRIAGE_LEVELS[2],
 									DemandType.ArbitraryFeedForward,
-									feedforward1.calculate(stageMotor.getActiveTrajectoryVelocity()));
-						carriageMotor.set(ControlMode.MotionMagic,
-										  CARRIAGE_LEVELS[2],
-										  DemandType.ArbitraryFeedForward,
-										  feedforward2.calculate(carriageMotor.getActiveTrajectoryVelocity()));
-						};
+									feedforward2);
 				break;
 			case 3:
-				setter = () -> {
-						stageMotor.set(ControlMode.MotionMagic,
-									STAGE_LEVELS[1],
+				stageMotor.set(ControlMode.MotionMagic,
+							STAGE_LEVELS[1],
+							DemandType.ArbitraryFeedForward,
+							feedforward1);
+				carriageMotor.set(ControlMode.MotionMagic,
+									CARRIAGE_LEVELS[2],
 									DemandType.ArbitraryFeedForward,
-									feedforward1.calculate(stageMotor.getActiveTrajectoryVelocity()));
-						carriageMotor.set(ControlMode.MotionMagic,
-											CARRIAGE_LEVELS[2],
-											DemandType.ArbitraryFeedForward,
-											feedforward2.calculate(carriageMotor.getActiveTrajectoryVelocity()));
-						};
+									feedforward2);
 				break;
 		}
 	}
 
-	@Override	
+	@Override
 	public void periodic() {
 		if (limitSwitch1.get()) {
 			stageMotor.setSelectedSensorPosition(0.0);
@@ -165,8 +142,27 @@ public class LiftSubsystem extends SubsystemBase {
 		if (limitSwitch2.get()) {
 			carriageMotor.setSelectedSensorPosition(0.0);
 		}
-		if (!override) {
-			setter.run();
-		}
+	}
+
+	@Override
+	public void initSendable(SendableBuilder builder) {
+		super.initSendable(builder);
+		builder.addDoubleProperty("ff1", () -> feedforward1, (value) -> feedforward1 = value);
+		builder.addDoubleProperty("accel1", () -> accel1, (value) -> accel1 = value);
+		builder.addDoubleProperty("maxV1", () -> maxV1, (value) -> maxV1 = value);
+		builder.addDoubleProperty("kP1", () -> kP1, (value) -> kP1 = value);
+		builder.addDoubleProperty("kI1", () -> kI1, (value) -> kI1 = value);
+		builder.addDoubleProperty("kD1", () -> kD1, (value) -> kD1 = value);
+		builder.addDoubleProperty("ff2", () -> feedforward2, (value) -> feedforward2 = value);
+		builder.addDoubleProperty("accel2", () -> accel2, (value) -> accel2 = value);
+		builder.addDoubleProperty("maxV2", () -> maxV2, (value) -> maxV2 = value);
+		builder.addDoubleProperty("kP2", () -> kP2, (value) -> kP2 = value);
+		builder.addDoubleProperty("kI2", () -> kI2, (value) -> kI2 = value);
+		builder.addDoubleProperty("kD2", () -> kD2, (value) -> kD2 = value);
+		builder.addDoubleProperty("STAGE_LEVELS[0]", () -> STAGE_LEVELS[0], (value) -> STAGE_LEVELS[0] = value);
+		builder.addDoubleProperty("STAGE_LEVELS[1]", () -> STAGE_LEVELS[1], (value) -> STAGE_LEVELS[1] = value);
+		builder.addDoubleProperty("CARRIAGE_LEVELS[0]", () -> CARRIAGE_LEVELS[0], (value) -> CARRIAGE_LEVELS[0] = value);
+		builder.addDoubleProperty("CARRIAGE_LEVELS[1]", () -> CARRIAGE_LEVELS[1], (value) -> CARRIAGE_LEVELS[1] = value);
+		builder.addDoubleProperty("CARRIAGE_LEVELS[2]", () -> CARRIAGE_LEVELS[2], (value) -> CARRIAGE_LEVELS[2] = value);
 	}
 }
