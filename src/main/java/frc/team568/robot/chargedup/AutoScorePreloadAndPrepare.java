264b6576b9b6a2dfaaaae8f23c081e8398148084
Danny Tix <dannytix@gmail.com>
2023-02-22 00:13:58 -0900

    Remove unused imports

--- src/main/java/frc/team568/robot/chargedup/AutoScorePreloadAndPrepare.java
+++ src/main/java/frc/team568/robot/chargedup/AutoScorePreloadAndPrepare.java
@@ -4,10 +4,8 @@ import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 
-import edu.wpi.first.wpilibj.BuiltInAccelerometer;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public class AutoScorePreloadAndPrepare extends SequentialCommandGroup{
 	PathPlannerTrajectory path = PathPlanner.loadPath("LeftScorePreloadAndExit", new PathConstraints(4.0, 3.0));
--- src/main/java/frc/team568/robot/chargedup/RobotContainer.java
+++ src/main/java/frc/team568/robot/chargedup/RobotContainer.java
@@ -6,14 +6,10 @@ import com.pathplanner.lib.auto.PIDConstants;
 import com.pathplanner.lib.auto.SwerveAutoBuilder;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RepeatCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.team568.robot.subsystems.AprilTags;
 
 final class RobotContainer {
 	CommandXboxController controller1;
