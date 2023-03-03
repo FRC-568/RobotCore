4bd19792157a354857e82e1bb33f2a81783e6830
consla <conslaaa@proton.me>
2023-03-02 20:19:59 -0900

    Divide drive motor output by 60 (unit conversion issues?)

--- src/main/java/frc/team568/robot/chargedup/Constants.java
+++ src/main/java/frc/team568/robot/chargedup/Constants.java
@@ -6,7 +6,7 @@ final class Constants {
 		
 		static final int kDriverControllerPort = 0;
 		static final int kCopilotControllerPort = 1;
-		static final double kControllerDeadband = 0.02; // inputs below are ignored; above are re-scaled from deadband offset.
+		static final double kControllerDeadband = 0.1; // inputs below are ignored; above are re-scaled from deadband offset.
 		static final double kAxisSlewRate = 3; // Controller axis max rate of change in units per second.
 
 	}
@@ -16,7 +16,7 @@ final class Constants {
 		static final int kEncoderResolution = 4096; // counts per rotation of turning motor
 		static final double kWheelRadius = 0.047625; // 3.75 inch wheels on mk4i
 		static final double kWheelCircumference = 2 * Math.PI * kWheelRadius;
-		static final double kMaxSpeed = 3.0; // 3 meters per second - 2.5 is probably more reliable / safe
+		static final double kMaxSpeed = 2.5; // 3 meters per second - 2.5 is probably more reliable / safe
 		static final double kMaxDriveRpm = kMaxSpeed / kWheelCircumference * 60; // Max drive speed in RPM
 		static final double kMaxDriveAcceleration = kMaxDriveRpm / 2.0; // Max velocity after 1s
 		static final double kMaxSpinRate = 4 * Math.PI; // 2 rotations per second
