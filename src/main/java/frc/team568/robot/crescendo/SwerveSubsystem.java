8fef536c44c6b1f4481b0523fb7256ac4b38d2db
NinjaO9 <davidmontesvilanova@gmail.com>
2024-01-20 13:56:38 -0900

    Experimenting with yagsl
    Co-authored-by: Sleepy <1234sleepy@users.noreply.github.com>
    Co-authored-by: dannytix <dannytix@users.noreply.github.com>
    Co-authored-by: Lafaye <LafayeEH@users.noreply.github.com>

--- src/main/java/frc/team568/robot/crescendo/SwerveSubsystem.java
+++ src/main/java/frc/team568/robot/crescendo/SwerveSubsystem.java
@@ -4,18 +4,11 @@
 
 package frc.team568.robot.crescendo;
 
-import static frc.team568.robot.crescendo.Constants.SwerveConstants.kBackOffset;
-import static frc.team568.robot.crescendo.Constants.SwerveConstants.kBackRot;
-import static frc.team568.robot.crescendo.Constants.SwerveConstants.kDrivePidChannel;
-import static frc.team568.robot.crescendo.Constants.SwerveConstants.kFrontOffset;
-import static frc.team568.robot.crescendo.Constants.SwerveConstants.kFrontRot;
-import static frc.team568.robot.crescendo.Constants.SwerveConstants.kLeftOffset;
-import static frc.team568.robot.crescendo.Constants.SwerveConstants.kLeftRot;
 import static frc.team568.robot.crescendo.Constants.SwerveConstants.kMaxSpeed;
-import static frc.team568.robot.crescendo.Constants.SwerveConstants.kRightOffset;
-import static frc.team568.robot.crescendo.Constants.SwerveConstants.kRightRot;
 
-import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import java.io.File;
+import java.io.IOException;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -26,20 +19,17 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import swervelib.SwerveDrive;
+import swervelib.parser.SwerveParser;
+import swervelib.SwerveModule;
 
 class SwerveSubsystem extends SubsystemBase {
 	protected static final String FIELD_REL_KEY = "Field Relative";
 
-	private final SwerveModule[] m_modules;
-	private final SwerveDriveKinematics m_kinematics;
 	private final ADXRS450_Gyro m_gyro = new ADXRS450_Gyro();
-
-	final ShuffleboardTab configTab;
 
 	//private GenericEntry[] cancoderOffsets;
 	//private double[] cancoderPrevOffsets;
@@ -49,13 +39,7 @@ class SwerveSubsystem extends SubsystemBase {
 
 	private GenericEntry _fieldRelative;
 
-	PIDConfig drivePID, turnPID;
-
-	// TO-DO: set relative cam pose to robot
-	// private final AprilTags apriltag = new AprilTags("photonvision", new Translation3d(0.0, 0.0, 0.0),
-	// 		new Rotation3d(0.0, 0.0, 0.0));
-
-	private final SwerveDrivePoseEstimator m_estimator;
+	protected SwerveDrive drive;
 
 	private boolean slowMode = false;
 	private double slowMultiplier = 0.25;
@@ -63,20 +47,12 @@ class SwerveSubsystem extends SubsystemBase {
 	private double speedMultiplier = 1.0;
 
 	public SwerveSubsystem(Pose2d startingPose) {
-		m_modules = new SwerveModule[] {
-				new SwerveModule(1, 2, 1, new Translation2d(kFrontOffset, 0), kFrontRot),
-				new SwerveModule(3, 4, 2, new Translation2d(0, kLeftOffset), kLeftRot),
-				new SwerveModule(5, 6, 3, new Translation2d(0, -kRightOffset), kRightRot),
-				new SwerveModule(7, 8, 4, new Translation2d(-kBackOffset, 0), kBackRot)
-		};
-		m_kinematics = new SwerveDriveKinematics(getModuleLocations());
-		m_estimator = new SwerveDrivePoseEstimator(m_kinematics, getHeading(), getModulePositions(), startingPose);
-
-		configTab = setupConfigTab();
-
-		for (ModuleIndex ind : ModuleIndex.values())
-			addChild(ind.name() + " module", m_modules[ind.index]);
-		addChild("Gyro", (ADXRS450_Gyro)m_gyro);
+		try {
+			drive = new SwerveParser(new File(Filesystem.getDeployDirectory(),"swerve")).createSwerveDrive(kMaxSpeed);
+		} catch(IOException e) {
+			DriverStation.reportError(e.getMessage(),e.getStackTrace());
+			throw new RuntimeException("Cannot create SwerveSubsystem: cannot read config files.");
+		}
 	}
 
 	/**
@@ -106,16 +82,12 @@ class SwerveSubsystem extends SubsystemBase {
 				: new ChassisSpeeds(xSpeed, ySpeed, rot));
 	}
 
-	public void setModuleStates(ChassisSpeeds outChassisSpeeds) {
-		targetTrajectory = new Translation2d(outChassisSpeeds.vxMetersPerSecond, outChassisSpeeds.vyMetersPerSecond);
-		targetRotation = new Rotation2d(outChassisSpeeds.omegaRadiansPerSecond);
-		var swerveModuleStates = m_kinematics.toSwerveModuleStates(outChassisSpeeds);
-		SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, kMaxSpeed);
-		setModuleStates(swerveModuleStates);
+	public void setModuleStates(ChassisSpeeds chassisSpeeds) {
+		drive.setChassisSpeeds(chassisSpeeds);
 	}
 
 	public ChassisSpeeds getChassisSpeeds() {
-		return m_kinematics.toChassisSpeeds(getModuleStates());
+		return drive.kinematics.toChassisSpeeds(getModuleStates());
 	}
 
 	public void toggleSlowMode() {
@@ -136,67 +108,47 @@ class SwerveSubsystem extends SubsystemBase {
 	}
 
 	public void setModuleStates(SwerveModuleState[] swerveModuleStates) {
-		if (swerveModuleStates == null || swerveModuleStates.length < m_modules.length)
+		setModuleStates(swerveModuleStates, true);
+	}
+
+	public void setModuleStates(SwerveModuleState[] swerveModuleStates, boolean isOpenLoop) {
+		if (swerveModuleStates == null || swerveModuleStates.length < drive.getModules().length)
 			throw new RuntimeException("Invalid SwerveModuleState size");
 
-		for (int i = 0; i < m_modules.length; i++)
-			m_modules[i].setDesiredState(swerveModuleStates[i]);
+		drive.setModuleStates(swerveModuleStates, isOpenLoop);
 	}
+
 
 	protected SwerveModule[] getModules() {
-		return m_modules.clone();
+		return drive.getModules();
 	}
 
-	protected SwerveModuleState[] getModuleStates() {
-		var states = new SwerveModuleState[m_modules.length];
-		for (int i = 0; i < m_modules.length; i++) {
-			states[i].angle = m_modules[i].getPosition().angle;
-			states[i].speedMetersPerSecond = m_modules[i].m_driveEncoder.getVelocity();
-		}
-		return states;
+	public SwerveModuleState[] getModuleStates() {
+		return drive.getStates();
 	}
 
 	protected SwerveModulePosition[] getModulePositions() {
-		var positions = new SwerveModulePosition[m_modules.length];
-		for (int i = 0; i < m_modules.length; i++)
-			positions[i] = m_modules[i].getPosition();
-		return positions;
+		return drive.getModulePositions();
 	}
-
-	protected Translation2d[] getModuleLocations() {
-		var locations = new Translation2d[m_modules.length];
-		for (int i = 0; i < m_modules.length; i++)
-			locations[i] = m_modules[i].location;
-		return locations;
-	}
-
+/* 
 	public Rotation2d getHeading() {
 		return m_gyro.getRotation2d();
 	}
 
-	public void resetHeading() {
+	/*public void resetHeading() {
 		m_gyro.reset();
 	}
-
+*/
 	public SwerveDriveKinematics getKinematics() {
-		return m_kinematics;
+		return drive.kinematics;
 	}
 
 	public Pose2d getPose() {
-		return m_estimator.getEstimatedPosition();
+		return drive.getPose();
 	}
 
 	public void resetPose(Pose2d pose) {
-		m_estimator.resetPosition(getHeading(), getModulePositions(), pose);
-	}
-
-	/** Updates the field relative position of the robot. */
-	public void updatePose() {
-		m_estimator.update(m_gyro.getRotation2d(), getModulePositions());
-		// Optional<EstimatedRobotPose> camPose = apriltag.getEstimatedPose();
-		// if (camPose.isPresent()) {
-		// 	m_estimator.addVisionMeasurement(camPose.get().estimatedPose.toPose2d(), camPose.get().timestampSeconds);
-		// }
+		drive.resetOdometry(pose);
 	}
 
 	public boolean isFieldRelative() {
@@ -210,116 +162,7 @@ class SwerveSubsystem extends SubsystemBase {
 	public boolean toggleFieldRelative() {
 		boolean fr = !isFieldRelative();
 		setFieldRelative(fr);
-		m_gyro.reset();
 		return fr;
-	}
-
-	private void updateConfig() {
-		if (drivePID.isDirty()) {
-			for (var module : m_modules) {
-				var controller = module.m_drivePIDController;
-				controller.setP(drivePID.getP(), kDrivePidChannel);
-				controller.setI(drivePID.getI(), kDrivePidChannel);
-				controller.setD(drivePID.getD(), kDrivePidChannel);
-				module.m_driveMotor.burnFlash();
-				module.setDriveFF(drivePID.getKv());
-			}
-			drivePID.clearDirtyFlag();
-		}
-
-		if (turnPID.isDirty()) {
-			for (var module : m_modules) {
-				var controller = module.m_turningPIDController;
-				controller.setP(turnPID.getP());
-				controller.setI(turnPID.getI());
-				controller.setD(turnPID.getD());
-			}
-			turnPID.clearDirtyFlag();
-		}
-
-		// 2024 migration - Is this necessary? More testing needed.
-		/*
-		for (int i = 0; i < cancoderOffsets.length; i++) {
-			var newValue = cancoderOffsets[i].get().getDouble();
-			if (newValue != cancoderPrevOffsets[i]) {
-				m_modules[i].m_turningEncoder.configMagnetOffset(newValue);
-				m_modules[i].m_turningEncoder.setPositionToAbsolute();
-				cancoderPrevOffsets[i] = newValue;
-			}
-		}
-		*/
-	}
-
-	@Override
-	public void periodic() {
-		updateConfig();
-		updatePose();
-	}
-
-	private ShuffleboardTab setupConfigTab() {
-		ShuffleboardTab configTab = Shuffleboard.getTab("Swerve Config");
-		ShuffleboardLayout layout;
-		double kP, kI, kD, kS, kV;
-		GenericEntry entryP, entryI, entryD, entryKs, entryKv;
-
-		// Max and Maria want lift position, speed, velocity, camera, auto selection, and a "normal dashboard"
-		// Add drive motor settings to their own layout
-		layout = configTab.getLayout("Drive", BuiltInLayouts.kList).withPosition(0, 0).withSize(1, 2);
-
-		var driveController = m_modules[0].m_drivePIDController;
-		kP = driveController.getP(kDrivePidChannel);
-		kI = driveController.getI(kDrivePidChannel);
-		kD = driveController.getD(kDrivePidChannel);
-		kS = 0.0;
-		kV = m_modules[0].getDriveFF();
-
-
-		entryP = layout.addPersistent("kP", kP).withPosition(0, 0).withSize(1, 1).getEntry();
-		entryI = layout.addPersistent("kI", kI).withPosition(0, 1).withSize(1, 1).getEntry();
-		entryD = layout.addPersistent("kD", kD).withPosition(0, 2).withSize(1, 1).getEntry();
-		entryKs = layout.addPersistent("kS", kS).withPosition(0, 3).withSize(1, 1).getEntry();
-		entryKv = layout.addPersistent("kV", kV).withPosition(0, 4).withSize(1, 1).getEntry();
-
-		this.drivePID = new PIDConfig(entryP, entryI, entryD, entryKs, entryKv);
-
-		// Add turning motor settings to their own layout
-		layout = configTab.getLayout("Turn", BuiltInLayouts.kList).withPosition(1, 1).withSize(1, 2);
-
-		var turnController = m_modules[0].m_turningPIDController;
-		kP = turnController.getP();
-		kI = turnController.getI();
-		kD = turnController.getD();
-		kS = m_modules[0].getTurnKs();
-		kV = m_modules[0].getTurnKv();
-
-		entryP = layout.addPersistent("kP", kP).withPosition(1, 0).withSize(1, 1).getEntry();
-		entryI = layout.addPersistent("kI", kI).withPosition(1, 1).withSize(1, 1).getEntry();
-		entryD = layout.addPersistent("kD", kD).withPosition(1, 2).withSize(1, 1).getEntry();
-		entryKs = layout.addPersistent("kS", kS).withPosition(1, 3).withSize(1, 1).getEntry();
-		entryKv = layout.addPersistent("kV", kV).withPosition(1, 4).withSize(1, 1).getEntry();
-
-		this.turnPID = new PIDConfig(entryP, entryI, entryD, entryKs, entryKv);
-
-		// Field-relative controls
-		_fieldRelative = configTab.addPersistent(FIELD_REL_KEY, true).getEntry();
-
-		// Add section for CANCoder settings
-		layout = configTab.getLayout("CANCoders", BuiltInLayouts.kGrid).withPosition(2, 2).withSize(2, 2);
-
-		//cancoderOffsets = new GenericEntry[m_modules.length];
-		//cancoderPrevOffsets = new double[m_modules.length];
-
-		// 2024 Migration - does this work? Need a better way to do this in Phoneix6
-		/*
-		for (int i = 0; i < m_modules.length; i++) {
-			double encValue = m_modules[i].m_turningEncoder.configGetMagnetOffset();
-			String modName = ModuleIndex.byIndex(i).name();
-			cancoderOffsets[i] = layout.addPersistent(modName + " Offset", encValue).getEntry();
-			cancoderPrevOffsets[i] = encValue;
-		}
-		*/
-
-		return configTab;
 	}
 
 	@Override
@@ -327,130 +170,6 @@ class SwerveSubsystem extends SubsystemBase {
 		super.initSendable(builder);
 
 		builder.addBooleanProperty(FIELD_REL_KEY, this::isFieldRelative, this::setFieldRelative);
-		builder.addDoubleProperty("left y-axis", () -> OI.Axis.swerveForward.getAsDouble(), null);
-		builder.addDoubleProperty("left x-axis", () -> OI.Axis.swerveLeft.getAsDouble(), null);
-		builder.addDoubleProperty("right x-axis", () -> OI.Axis.swerveCCW.getAsDouble(), null);
-		builder.addBooleanProperty("forward above deadzone", () -> OI.Axis.swerveForward.getAsDouble() >= Constants.OIConstants.kAxisSlewRate ? true : false, null);
-		builder.addBooleanProperty("strafe above deadzone", () -> OI.Axis.swerveLeft.getAsDouble() >= Constants.OIConstants.kAxisSlewRate ? true : false, null);
-		builder.addBooleanProperty("turn above deadzone", () -> OI.Axis.swerveCCW.getAsDouble() >= Constants.OIConstants.kAxisSlewRate ? true : false, null);
-		builder.addDoubleProperty("target x speed", () -> getTargetTrajectory().getX(), null);
-		builder.addDoubleProperty("target y speed", () -> getTargetTrajectory().getY(), null);
-		builder.addDoubleProperty("target rot speed", () -> getTargetRotation().getRadians(), null);
-		builder.addDoubleProperty("actual wheel speed 1", () -> getModules()[0].getState().speedMetersPerSecond, null);
-		builder.addDoubleProperty("actual wheel speed 2", () -> getModules()[1].getState().speedMetersPerSecond, null);
-		builder.addDoubleProperty("actual wheel speed 3", () -> getModules()[2].getState().speedMetersPerSecond, null);
-		builder.addDoubleProperty("actual wheel speed 4", () -> getModules()[3].getState().speedMetersPerSecond, null);
-		builder.addDoubleProperty("actual wheel angle 1", () -> getModules()[0].getState().angle.getDegrees(), null);
-		builder.addDoubleProperty("actual wheel angle 2", () -> getModules()[1].getState().angle.getDegrees(), null);
-		builder.addDoubleProperty("actual wheel angle 3", () -> getModules()[2].getState().angle.getDegrees(), null);
-		builder.addDoubleProperty("actual wheel angle 4", () -> getModules()[3].getState().angle.getDegrees(), null);
-		builder.addDoubleProperty("wheel position", () -> getModules()[0].m_driveEncoder.getPosition(), null);
-	}
-
-	public final class PIDConfig {
-		private final GenericEntry entryP, entryI, entryD, entryKs, entryKv;
-		private double lastP = -1, lastI = -1, lastD = -1, lastKs = -1, lastKv = -1;
-
-		private PIDConfig(final GenericEntry p, final GenericEntry i, final GenericEntry d, final GenericEntry kS, final GenericEntry kV) {
-			this.entryP = p;
-			this.entryI = i;
-			this.entryD = d;
-			this.entryKs = kS;
-			this.entryKv = kV;
-		}
-
-		public double getP() {
-			return entryP.get().getDouble();
-		}
-
-		public boolean setP(double value) {
-			if (value == getP())
-				return false;
-
-			entryP.setDouble(value);
-			return true;
-		}
-
-		public double getI() {
-			return entryI.get().getDouble();
-		}
-
-		public boolean setI(double value) {
-			if (value == getI())
-				return false;
-
-			entryI.setDouble(value);
-			return true;
-		}
-
-		public double getD() {
-			return entryD.get().getDouble();
-		}
-
-		public boolean setD(double value) {
-			if (value == getD())
-				return false;
-
-			entryD.setDouble(value);
-			return true;
-		}
-
-		public double getKs() {
-			return entryKs.get().getDouble();
-		}
-
-		public boolean setKs(double value) {
-			if (value == getD())
-				return false;
-
-			entryKs.setDouble(value);
-			return true;
-		}
-
-		public double getKv() {
-			return entryKv.get().getDouble();
-		}
-
-		public boolean setKv(double value) {
-			if (value == getKv())
-				return false;
-
-			entryKv.setDouble(value);
-			return true;
-		}
-
-		private boolean isDirty() {
-			return lastP != getP() || lastI != getI() || lastD != getD() || lastKs != getKs() || lastKv != getKv();
-		}
-
-		private void clearDirtyFlag() {
-			lastP = getP();
-			lastI = getI();
-			lastD = getD();
-			lastKs = getKs();
-			lastKv = getKv();
-		}
-	}
-
-	public enum ModuleIndex {
-		FRONT(0),
-		LEFT(1),
-		RIGHT(2),
-		BACK(3);
-
-		public final int index;
-
-		ModuleIndex(int index) {
-			this.index = index;
-		}
-
-		public static ModuleIndex byIndex(int indexValue) {
-			for (var m : values()) {
-				if (m.index == indexValue)
-					return m;
-			}
-			return null;
-		}
 	}
 
 }
