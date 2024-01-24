828be638e13afea73cda9db54450f2bf9f849368
Danny Tix <dannytix@gmail.com>
2024-01-23 23:17:07 -0900

    Point build to cresecendo package

--- src/main/java/frc/team568/robot/Main.java
+++ src/main/java/frc/team568/robot/Main.java
@@ -1,7 +1,7 @@
 package frc.team568.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
-import frc.team568.robot.chargedup.Robot;
+import frc.team568.robot.crescendo.Robot;
 
 public final class Main {
 	
