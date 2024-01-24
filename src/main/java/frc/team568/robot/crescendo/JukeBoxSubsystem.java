3541a46f24287ffabc16ff881ec3c7759a3519bb
AS1624 <thebest1dudes@gmail.com>
2024-01-23 20:14:14 -0900

    add cansparkmax intake motor to jukebox

--- src/main/java/frc/team568/robot/crescendo/JukeBoxSubsystem.java
+++ src/main/java/frc/team568/robot/crescendo/JukeBoxSubsystem.java
@@ -7,40 +7,43 @@ import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class JukeBoxSubsystem extends SubsystemBase {
     //=== motors ===
-	private TalonFX leftOutakeMotor;
-    private TalonFX rightOutakeMotor;
+	private TalonFX leftOuttakeMotor;
+    private TalonFX rightOuttakeMotor;
 	private CANSparkMax intakeMotor;
 
 
 	boolean override = false;
 
-	public JukeBoxSubsystem(int leftOutakeMotorPort, int rightOutakeMotorPort, int intakeMotorPort) {
-		leftOutakeMotor = new TalonFX(leftOutakeMotorPort);
-		addChild("leftOutakeMotor", leftOutakeMotor);
+	public JukeBoxSubsystem(int leftOuttakeMotorPort, int rightOuttakeMotorPort, int intakeMotorPort) {
+		leftOuttakeMotor = new TalonFX(leftOuttakeMotorPort);
+		addChild("leftOuttakeMotor", leftOuttakeMotor);
 
-		rightOutakeMotor = new TalonFX(rightOutakeMotorPort);
-		addChild("rightOutakeMotor", rightOutakeMotor);
+		rightOuttakeMotor = new TalonFX(rightOuttakeMotorPort);
+		addChild("rightOuttakeMotor", rightOuttakeMotor);
+
 		
-		// intakeMotor = new CANSparkMax(intakeMotorPort);
-		// addChild("intakeMotor", intakeMotor);
+		intakeMotor = new CANSparkMax(intakeMotorPort, MotorType.kBrushless);
+		//addChild("intakeMotor", intakeMotor);
+		//TODO: thats important
 		
 		MotorOutputConfigs currentConfigs = new MotorOutputConfigs();
 		currentConfigs.Inverted = InvertedValue.Clockwise_Positive; //TODO: reverse directions based on design
 
-		leftOutakeMotor.getConfigurator().apply(currentConfigs);
-		rightOutakeMotor.setControl(new Follower(leftOutakeMotor.getDeviceID(), true));
+		leftOuttakeMotor.getConfigurator().apply(currentConfigs);
+		rightOuttakeMotor.setControl(new Follower(leftOuttakeMotor.getDeviceID(), true));
 		intakeMotor.setInverted(true);
 
 		//TODO: Make invert intake motor based on design
 
 		//manning
-		//rightOutakeMotor.optimizeBusUtilization()
+		//rightOuttakeMotor.optimizeBusUtilization()
 		
 		
 		//=== pid configs ===
@@ -50,7 +53,7 @@ public class JukeBoxSubsystem extends SubsystemBase {
 		slot0Configs.kI = 0; //no output for integrated error
 		slot0Configs.kD = 0; //A velocity of 1 rps results in 0.1 V output at a setting of 0.1
 
-		leftOutakeMotor.getConfigurator().apply(slot0Configs);
+		leftOuttakeMotor.getConfigurator().apply(slot0Configs);
         
 	}
 
