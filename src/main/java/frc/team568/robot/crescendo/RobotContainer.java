2fc94fb5f69c54ce0e9320c345e8ee2aebe7b854
AkumaOff <142070440+1234sleepy@users.noreply.github.com>
2024-02-04 11:59:59 -0900

    Update code

--- src/main/java/frc/team568/robot/crescendo/RobotContainer.java
+++ src/main/java/frc/team568/robot/crescendo/RobotContainer.java
@@ -67,15 +67,28 @@ final class RobotContainer {
 		setupConfigTab();
 
 		pd = new PowerDistribution(1, ModuleType.kRev);
+
+		jukebox.setDefaultCommand(new InstantCommand(
+			() -> {
+				jukebox.setOuttakeSpeed(controller1.getLeftTriggerAxis(),controller1.getRightTriggerAxis());
+				jukebox.setIntakeSpeed(controller1.getLeftY());
+			}
+		));
+		
 	}
 
 	public void configureButtonBindings() {
 		OI.Button.fieldRelativeControl.onTrue(new InstantCommand(drive::toggleFieldRelative));
-		controller1.leftBumper().whileTrue(new Intake(jukebox, pivot)); //intake until bumper is released
-		controller1.b().onTrue(new ScoreAmp(jukebox, pivot));
-		controller1.x().onTrue(new ScoreSpeaker(jukebox, pivot));
-		controller1.y().onTrue(new InstantCommand(() -> pivot.setAngle(0)));
-		controller1.a().onTrue(new InstantCommand(() -> pivot.setAngle(90)));
+		// controller1.leftBumper().whileTrue(new Intake(jukebox, pivot)); //intake until bumper is released
+		// controller1.b().onTrue(new ScoreAmp(jukebox, pivot));
+		// controller1.x().onTrue(new ScoreSpeaker(jukebox, pivot));
+		// controller1.y().onTrue(new InstantCommand(() -> pivot.setAngle(0)));
+		// controller1.a().onTrue(new InstantCommand(() -> pivot.setAngle(90)));
+		//getRightTriggerAxis()
+		// controller1.rightTrigger().whileTrue(Commands.runEnd(() -> new Command(jukebox.setOuttakeSpeed(controller1.getRightTriggerAxis(),controller1.getRightTriggerAxis())),() ->new Command(jukebox.setOuttakeSpeed(0,0))));
+		// controller1.leftTrigger().whileTrue(Commands.runEnd(() -> new Command(jukebox.setIntakeSpeed(controller1.getLeftTriggerAxis())),() ->new Command(jukebox.setIntakeSpeed(0))));
+	
+		
 	}
 
 	public Command getAutonomousCommand() {
@@ -113,7 +126,6 @@ final class RobotContainer {
 		.withPosition(0, 0)
 		.withSize(1, 1)
 		.getEntry();
-		
 
 		
 		kiEntry = configTab.add("ki",0)
