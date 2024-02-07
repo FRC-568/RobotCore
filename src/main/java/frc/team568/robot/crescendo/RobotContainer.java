f7bce023ccd521f14bcc171ebbaac53b83cde03a
AS1624 <thebest1dudes@gmail.com>
2024-02-06 20:50:23 -0900

    add code

--- src/main/java/frc/team568/robot/crescendo/RobotContainer.java
+++ src/main/java/frc/team568/robot/crescendo/RobotContainer.java
@@ -20,6 +20,8 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.team568.robot.crescendo.subsystem.JukeboxSubsystem;
+import frc.team568.robot.crescendo.subsystem.PivotSubsystem;
 import swervelib.telemetry.SwerveDriveTelemetry;
 
 final class RobotContainer {
