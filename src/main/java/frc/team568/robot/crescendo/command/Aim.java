8a6c1364dac9f92682afa9253e89189b9b478191
FRC-568 Driver Station <frcteam568@gmail.com>
2024-02-10 10:49:25 -0900

    add aim command

--- src/main/java/frc/team568/robot/crescendo/command/Aim.java
+++ src/main/java/frc/team568/robot/crescendo/command/Aim.java
@@ -1,7 +1,40 @@
 package frc.team568.robot.crescendo.command;
 
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.team568.robot.crescendo.subsystem.JukeboxSubsystem;
+import frc.team568.robot.crescendo.subsystem.PivotSubsystem;
 
 public class Aim extends Command{
 	
+	
+	PivotSubsystem pivot;
+	
+	DoubleSupplier distance;
+
+
+	public Aim(PivotSubsystem pivot){
+		this.pivot = pivot;
+	}
+
+	@Override
+	public void initialize() {
+		distance = () -> Math.random();
+	}
+
+	@Override
+	public void execute() {
+		pivot.aim(distance.getAsDouble());
+	}
+
+	@Override
+	public boolean isFinished() {
+		return false;
+	}
+
+	@Override
+	public void end(boolean interrupted) {
+		
+	}
 }
