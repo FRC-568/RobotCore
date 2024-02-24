6fc3937fca9e9ce1e2afb3b9b57527c4811735d6
FRC-568 Driver Station <frcteam568@gmail.com>
2024-02-24 12:07:11 -0900

    for real this time

---
+++ src/main/java/frc/team568/robot/crescendo/command/HomePivot.java
@@ -0,0 +1,31 @@
+package frc.team568.robot.crescendo.command;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.team568.robot.crescendo.subsystem.JukeboxSubsystem;
+import frc.team568.robot.crescendo.subsystem.PivotSubsystem;
+
+public class HomePivot extends Command {
+	PivotSubsystem pivot;
+
+	public HomePivot(PivotSubsystem pivot){
+		this.pivot = pivot;
+	}
+
+	@Override
+	public void initialize() {
+		pivot.setVoltage(1);
+	}
+
+	@Override
+	public void execute() {
+	}
+
+	@Override
+	public boolean isFinished() {
+		return pivot.getSwitch();
+	}
+
+	@Override
+	public void end(boolean interrupted) {
+	}
+}
