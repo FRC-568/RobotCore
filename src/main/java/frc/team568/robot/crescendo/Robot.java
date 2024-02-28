ba5c9b19f772ff9f2b461aa2979f21c5ea6c669b
AkumaOff <142070440+1234sleepy@users.noreply.github.com>
2024-02-27 15:21:08 -0900

    Add ColorSensor

--- src/main/java/frc/team568/robot/crescendo/Robot.java
+++ src/main/java/frc/team568/robot/crescendo/Robot.java
@@ -4,10 +4,17 @@
 
 package frc.team568.robot.crescendo;
 
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorSensorV3;
+
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import swervelib.telemetry.SwerveDriveTelemetry;
@@ -16,6 +23,13 @@ public class Robot extends TimedRobot {
 	private RobotContainer container;
 	private Command m_autonomousCommand;
 
+	private final I2C.Port i2cPort = I2C.Port.kOnboard;
+	private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+	private final ColorMatch m_colorMatcher = new ColorMatch();
+
+	//add color, i dont know values
+	private final Color kTarget = new Color();
+
 	@Override
 	public void robotInit() {
 		container = new RobotContainer();
@@ -23,6 +37,8 @@ public class Robot extends TimedRobot {
 			Shuffleboard.selectTab(OI.autoTab.getTitle());
 		else
 			SwerveDriveTelemetry.verbosity = SwerveDriveTelemetry.TelemetryVerbosity.HIGH;
+		
+		m_colorMatcher.addColorMatch(kTarget);
 	}
 
 	@Override
@@ -70,6 +86,17 @@ public class Robot extends TimedRobot {
 	@Override
 	public void robotPeriodic() {
 		CommandScheduler.getInstance().run();
+
+		Color detectedColor = m_colorSensor.getColor();
+		String colorString;
+    	ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+		if (match.color == kTarget) {
+			colorString = "Note";
+		}else {
+			colorString = "Unknown";
+		}
+
+		SmartDashboard.putString("Detected Color", colorString);
 	}
 
 	@Override
