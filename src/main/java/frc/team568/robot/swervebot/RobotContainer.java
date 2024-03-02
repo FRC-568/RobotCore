983e2593675bcd75a56d2cf944befc21568e7316
Danny Tix <dannytix@gmail.com>
2024-03-01 22:38:43 -0900

    Split old swervebot testing from crescendo

--- src/main/java/frc/team568/robot/swervebot/RobotContainer.java
+++ src/main/java/frc/team568/robot/swervebot/RobotContainer.java
@@ -1,4 +1,4 @@
-package frc.team568.robot.crescendo;
+package frc.team568.robot.swervebot;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,77 +11,40 @@ import com.pathplanner.lib.util.ReplanningConfig;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
-import edu.wpi.first.wpilibj.shuffleboard.ComplexWidget;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-
-import frc.team568.robot.crescendo.subsystem.JukeboxSubsystem;
-import frc.team568.robot.crescendo.subsystem.PivotSubsystem;
-import frc.team568.robot.crescendo.subsystem.PneumaticSubsystem;
 import frc.team568.robot.crescendo.subsystem.VisionSubsystem;
 import frc.team568.robot.subsystems.SwerveSubsystem;
+import swervelib.telemetry.SwerveDriveTelemetry;
 
-import static frc.team568.robot.crescendo.Constants.SwerveConstants.kMaxSpeed;
-import static frc.team568.robot.crescendo.Constants.SwerveConstants.kWheelbaseRadius;
+import static frc.team568.robot.swervebot.Constants.SwerveConstants.kMaxSpeed;
+import static frc.team568.robot.swervebot.Constants.SwerveConstants.kWheelbaseRadius;
 
 public final class RobotContainer {
 	public final SwerveSubsystem drive;
-	public PivotSubsystem pivot /* = new PivotSubsystem(0, 0)*/;
-	public final JukeboxSubsystem jukebox = new JukeboxSubsystem(12, 11, 13);
-	public final VisionSubsystem vision = new VisionSubsystem();
+	public final VisionSubsystem vision;
 
 	Map<String, Command> eventMap = new HashMap<>();
 
-	// Auto tab objects
-	public AutoTab autoTab;
-	
-	// Driver tab objects
-	public DriverTab driverTab;
-	
-	// Config tab objects
-	public ConfigTab configTab;
-
-	//public FlywheelTab flywheelTab;
-
-	// Pneumatic Subsystem object
-	public PneumaticSubsystem pneumaticsub;
-
-	ComplexWidget enterButton;
-
 	PowerDistribution pd;
 
-
 	public RobotContainer() {
-		drive = new SwerveSubsystem("crescendo", kMaxSpeed);
+		drive = new SwerveSubsystem("swerve", kMaxSpeed);
 		drive.initDefaultCommand(OI.Axis.swerveForward, OI.Axis.swerveLeft, OI.Axis.swerveCCW);
+		SwerveDriveTelemetry.verbosity = SwerveDriveTelemetry.TelemetryVerbosity.MACHINE;
 		configurePathplanner();
 
-		//pivot.setDefaultCommand(new PivotSubsystemDefaultCommand(pivot));
-
+		vision = new VisionSubsystem();
 		vision.addPoseListener(est -> drive.addVisionMeasurement(est.estimatedPose.toPose2d(), est.timestampSeconds));
 		vision.startPoseListenerThread();
 
-		jukebox.initDefaultCommand(OI.Axis.intakeSpeed, OI.Axis.outtakeSpeedL, OI.Axis.outtakeSpeedR);
-
 		pd = new PowerDistribution(1, ModuleType.kRev);
-
-		//autoTab = new AutoTab(this);
-		driverTab = new DriverTab(this);
-		configTab = new ConfigTab(this);
-		//flywheelTab = new FlywheelTab(this);
-		pneumaticsub = new PneumaticSubsystem();
 
 		configureButtonBindings();
 	}
 
 	public void configureButtonBindings() {
 		OI.Button.fieldRelativeControl.onTrue(new InstantCommand(drive::toggleFieldRelative));
-		// OI.Button.intake.whileTrue(new Intake(jukebox, pivot)); //intake until bumper is released
-		// OI.Button.scoreAmp.onTrue(new ScoreAmp(jukebox, pivot));
-		// OI.Button.scoreSpeaker.onTrue(new ScoreSpeaker(jukebox, pivot));
-		// OI.Button.pivotDown.onTrue(new InstantCommand(() -> pivot.setAngle(0)));
-		// OI.Button.pivotUp.onTrue(new InstantCommand(() -> pivot.setAngle(90)));
-		OI.Button.pneumaticstateswitch.onTrue(new InstantCommand(pneumaticsub::SwitchState));
 		OI.driverController.back().onTrue(AutoBuilder.buildAuto("Backwards Line"));
 	}
 
@@ -108,6 +71,6 @@ public final class RobotContainer {
 	}
 
 	public Command getAutonomousCommand() {
-		return autoTab.chooser.getSelected();
+		return null;
 	}
 }
