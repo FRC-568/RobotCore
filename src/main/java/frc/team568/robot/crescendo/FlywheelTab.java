df1427e3628bc6cb74ec06621a19e1cb7bf67e47
FRC-568 Driver Station <frcteam568@gmail.com>
2024-03-06 19:46:52 -0900

    merge again (adds shoot command back in)

---
+++ src/main/java/frc/team568/robot/crescendo/FlywheelTab.java
@@ -0,0 +1,15 @@
+package frc.team568.robot.crescendo;
+
+public class FlywheelTab {
+	public FlywheelTab(RobotContainer container) {
+		
+		var jukebox = container.jukebox;
+ 
+		
+		OI.flywheelTab.addDouble("left Flywheel", jukebox::getLeftVelo).withPosition(1,1);
+		OI.flywheelTab.addDouble("right Flywheel", jukebox::getRightVelo).withPosition(3,1);
+		OI.flywheelTab.addDouble("sensor distance", jukebox::getDistance);
+		OI.flywheelTab.addBoolean("contains note", jukebox::hasNote);
+
+	}
+}
--- src/main/java/frc/team568/robot/crescendo/command/Shoot.java
+++ src/main/java/frc/team568/robot/crescendo/command/Shoot.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package frc.team568.robot.crescendo.command;
 
 import edu.wpi.first.wpilibj2.command.Command;
@@ -7,4 +8,39 @@ public class Shoot extends Command{
 	public Shoot(){
 		
 	}
+=======
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.team568.robot.crescendo.command;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import frc.team568.robot.crescendo.subsystem.JukeboxSubsystem;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class Shoot extends SequentialCommandGroup {
+  /** Creates a new Shoot. */
+  double outtakeStartTime;
+  public Shoot(JukeboxSubsystem jukebox) {
+    super();
+    addCommands(
+      new RunCommand(() -> {jukebox.setIntakeSpeed(-0.1);}).withTimeout(0.25),
+      new SpinUp(jukebox).withTimeout(3)
+      .raceWith(
+        new WaitCommand(1)
+        .andThen(new RunCommand(() -> { jukebox.setIntakeSpeed(1); }).withTimeout(3))
+      )
+    );
+  }
+>>>>>>> 7585408 (add shoot command)
 }
