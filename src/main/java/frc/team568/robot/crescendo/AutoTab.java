9bb7a8e1cd2ca0f527ea1c589be57b825732856a
NinjaO9 <davidmontesvilanova@gmail.com>
2024-03-17 18:27:19 -0800

    Reintroduced AutoTab

--- src/main/java/frc/team568/robot/crescendo/AutoTab.java
+++ src/main/java/frc/team568/robot/crescendo/AutoTab.java
@@ -2,27 +2,30 @@ package frc.team568.robot.crescendo;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.team568.robot.crescendo.command.Intake;
+import frc.team568.robot.crescendo.command.ScoreAmp;
+import frc.team568.robot.crescendo.subsystem.PneumaticSubsystem;
 
 public class AutoTab {
 
 	public SendableChooser<Command> chooser = new SendableChooser<>();
+	
+	public PneumaticSubsystem lift;
 
 	public AutoTab(RobotContainer container) {
 		var pivot = container.pivot;
-		var drive = container.drive;
 		var jukebox = container.jukebox;
-		//var dSolenoid = container.dSolenoid;
 		
-		//Command downpneumatic = new DownPneumatic(dSolenoid);
-		//Command intake = new Intake(jukebox, pivot);
-		//Command scoreamp = new ScoreAmp(jukebox, pivot);
-		//Command uppneumatic = new UpPneumatic(dSolenoid);
-		/* 
+		Command intake = new Intake(jukebox, pivot);
+		Command scoreamp = new ScoreAmp(jukebox, pivot);
+		Command togglepneumatic = lift.getToggleCommand();
+
 		chooser.setDefaultOption("Wait", null);
 		chooser.addOption("Intake", intake);
 		chooser.addOption("Score AMP", scoreamp);
+		chooser.addOption("Toggle Pneumatic", togglepneumatic);
+		
 		OI.autoTab.add("Auto Program", chooser);
-		*/
 	}
 
 }
